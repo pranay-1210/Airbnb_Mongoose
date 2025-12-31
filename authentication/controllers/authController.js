3266d4e564d8a76acf9950168d716a1230490582
Pranay <praveenpranay07@gmail.com>
2025-12-31 08:45:21 +0530

    Added User model with required fields, unique email and user type, created and saved new users during signup, and redirected users to login after successful registration.

--- authentication/controllers/authController.js
+++ authentication/controllers/authController.js
@@ -1,104 +1,118 @@
-
-const { check, validationResult } = require('express-validator');
-
+const { check, validationResult } = require("express-validator");
+const User = require("../models/User");
 
 exports.getLogin = (req, res, next) => {
-  res.render('auth/login', {pageTitle: "Login", isLoggedIn: false });
-}
+  res.render("auth/login", { pageTitle: "Login", isLoggedIn: false });
+};
 
 exports.getSignup = (req, res, next) => {
-  res.render('auth/signup', {pageTitle: "Signup", isLoggedIn: false });
-}
+  res.render("auth/signup", { pageTitle: "Signup", isLoggedIn: false });
+};
 
 exports.postLogin = (req, res, next) => {
-    req.session.isLoggedIn = true;
-    res.redirect('/');
-}
+  req.session.isLoggedIn = true;
+  res.redirect("/");
+};
 
 exports.postSignup = [
   // First name validator
-  check('firstName')
-  .notEmpty()
-  .withMessage('First name is mandatory')
-  .trim()
-  .isLength({ min: 2 })
-  .withMessage('First name must be at least 2 characters long')
-  .matches(/^[a-zA-Z\s]+$/)
-  .withMessage('First name can only contain letters and spaces'),
+  check("firstName")
+    .notEmpty()
+    .withMessage("First name is mandatory")
+    .trim()
+    .isLength({ min: 2 })
+    .withMessage("First name must be at least 2 characters long")
+    .matches(/^[a-zA-Z\s]+$/)
+    .withMessage("First name can only contain letters and spaces"),
 
-  
   // Last name validator
-  check('lastName')
-  .trim()
-  .matches(/^[a-zA-Z\s]*$/)
-  .withMessage('Last name can only contain letters and spaces'),
+  check("lastName")
+    .trim()
+    .matches(/^[a-zA-Z\s]*$/)
+    .withMessage("Last name can only contain letters and spaces"),
 
   // Email validator
-  check('email')
-  .isEmail()
-  .withMessage('Please enter a valid email address')
-  .normalizeEmail(),
+  check("email")
+    .isEmail()
+    .withMessage("Please enter a valid email address")
+    .normalizeEmail(),
 
   // Password validator
-  check('password')
-  .isLength({ min: 8 })
-  .withMessage('Password must be at least 8 characters long')
-  .matches(/[a-z]/)
-  .withMessage('Password must contain at least one lowercase letter')
-  .matches(/[A-Z]/)
-  .withMessage('Password must contain at least one uppercase letter')
-  .matches(/[0-9]/)
-  .withMessage('Password must contain at least one number')
-  .matches(/[!@#$%^&*?><=+]/)
-  .withMessage('Password must contain at least one special character')
-  .trim(),
+  check("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long")
+    .matches(/[a-z]/)
+    .withMessage("Password must contain at least one lowercase letter")
+    .matches(/[A-Z]/)
+    .withMessage("Password must contain at least one uppercase letter")
+    .matches(/[0-9]/)
+    .withMessage("Password must contain at least one number")
+    .matches(/[!@#$%^&*?><=+]/)
+    .withMessage("Password must contain at least one special character")
+    .trim(),
 
   // Confirm password validator
-  check('confirm_password')
-  .trim()
-  .custom((value, { req }) => {
-    if (value !== req.body.password) {
-      throw new Error('Confirm Password does not match Password');
-    }
-    return true;
-  }),
-  
+  check("confirm_password")
+    .trim()
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Confirm Password does not match Password");
+      }
+      return true;
+    }),
+
   // User type validator
-  check('userType')
-  .trim()
-  .notEmpty()
-  .withMessage('Please select a user type')
-  .isIn(['guest', 'host'])
-  .withMessage('Please select either host or guest'),
+  check("userType")
+    .trim()
+    .notEmpty()
+    .withMessage("Please select a user type")
+    .isIn(["guest", "host"])
+    .withMessage("Please select either host or guest"),
 
   // Terms and conditions validator
-  check('terms')
-  .notEmpty()
-  .withMessage('Please agree to the terms and conditions'),
-
+  check("terms")
+    .notEmpty()
+    .withMessage("Please agree to the terms and conditions"),
 
   (req, res, next) => {
-    console.log('User came for signup', req.body);
+    console.log("User came for signup", req.body);
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(422).render('auth/signup', 
-        {
-          pageTitle: 'Login',
-          isLoggedIn: false,
-          errorMessages: errors.array().map(err => err.msg),
-          oldInput: req.body ,  
-        });
+      return res.status(422).render("auth/signup", {
+        pageTitle: "Login",
+        isLoggedIn: false,
+        errorMessages: errors.array().map((err) => err.msg),
+        oldInput: req.body,
+      });
     }
 
+    // Save the user to the database
+    const { firstName, lastName, email, password, userType } = req.body;
+    const user = new User({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+      userType: userType,
+    });
 
-
-    res.redirect('/login');
+    user.save().then(result => {
+        console.log(result);
+        res.redirect("/login");
+      })
+      .catch(error => {
+        return res.status(422).render("auth/signup", {
+          pageTitle: "Login",
+          isLoggedIn: false,
+          errorMessages: [error],
+          oldInput: req.body,
+        })
+      }) 
   }
 ];
 
 exports.postLogout = (req, res, next) => {
-    req.session.destroy();
-    res.redirect('/login');
-
-}
+  req.session.destroy();
+  res.redirect("/login");
+};
--- authentication/models/User.js
+++ authentication/models/User.js
@@ -1,15 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+const homeSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  userType: { type: String, required: true, enum: ['guest', 'host'] },
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", homeSchema);
