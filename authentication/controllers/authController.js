b6c39001b4b9b0ce03ad33326c6d8f86b5d53ecc
Pranay <praveenpranay07@gmail.com>
2025-12-31 08:15:07 +0530

    Added POST signup handling, installed and used express-validator, validated user inputs like name, email, password, user type, and terms, and updated signup page to display validation errors and keep old values.

--- authentication/controllers/authController.js
+++ authentication/controllers/authController.js
@@ -1,3 +1,7 @@
+
+const { check, validationResult } = require('express-validator');
+
+
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {pageTitle: "Login", isLoggedIn: false });
 }
@@ -11,10 +15,87 @@ exports.postLogin = (req, res, next) => {
     res.redirect('/');
 }
 
-exports.postSignup = (req, res, next) => {
+exports.postSignup = [
+  // First name validator
+  check('firstName')
+  .notEmpty()
+  .withMessage('First name is mandatory')
+  .trim()
+  .isLength({ min: 2 })
+  .withMessage('First name must be at least 2 characters long')
+  .matches(/^[a-zA-Z\s]+$/)
+  .withMessage('First name can only contain letters and spaces'),
+
+  
+  // Last name validator
+  check('lastName')
+  .trim()
+  .matches(/^[a-zA-Z\s]*$/)
+  .withMessage('Last name can only contain letters and spaces'),
+
+  // Email validator
+  check('email')
+  .isEmail()
+  .withMessage('Please enter a valid email address')
+  .normalizeEmail(),
+
+  // Password validator
+  check('password')
+  .isLength({ min: 8 })
+  .withMessage('Password must be at least 8 characters long')
+  .matches(/[a-z]/)
+  .withMessage('Password must contain at least one lowercase letter')
+  .matches(/[A-Z]/)
+  .withMessage('Password must contain at least one uppercase letter')
+  .matches(/[0-9]/)
+  .withMessage('Password must contain at least one number')
+  .matches(/[!@#$%^&*?><=+]/)
+  .withMessage('Password must contain at least one special character')
+  .trim(),
+
+  // Confirm password validator
+  check('confirm_password')
+  .trim()
+  .custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error('Confirm Password does not match Password');
+    }
+    return true;
+  }),
+  
+  // User type validator
+  check('userType')
+  .trim()
+  .notEmpty()
+  .withMessage('Please select a user type')
+  .isIn(['guest', 'host'])
+  .withMessage('Please select either host or guest'),
+
+  // Terms and conditions validator
+  check('terms')
+  .notEmpty()
+  .withMessage('Please agree to the terms and conditions'),
+
+
+  (req, res, next) => {
     console.log('User came for signup', req.body);
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).render('auth/signup', 
+        {
+          pageTitle: 'Login',
+          isLoggedIn: false,
+          errorMessages: errors.array().map(err => err.msg),
+          oldInput: req.body ,  
+        });
+    }
+
+
+
     res.redirect('/login');
-}
+  }
+];
 
 exports.postLogout = (req, res, next) => {
     req.session.destroy();
